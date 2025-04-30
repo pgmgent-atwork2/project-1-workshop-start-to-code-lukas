e470744f24dee815b6bfc1994178a3ac49a32652
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-04-30 19:48:53 +0200

    added form to add songs but no functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-const playButton = document.querySelector(".play-button")
-const pauseButton = document.querySelector(".pause-button")
-const audioPlayer = document.getElementById('audio-player');
+const galleryItems = document.querySelectorAll(".gallery-item");
 
+galleryItems.forEach((item) => {
+    const playButton = item.querySelector(".play-button");
+    const pauseButton = item.querySelector(".pause-button");
+    const audioPlayer = item.querySelector("audio");
 
-playButton.addEventListener("click", () => {
-    audioPlayer.play();
-    playButton.classList.add("hidden")
-    pauseButton.classList.remove("hidden")
+    playButton.addEventListener("click", () => {
+        audioPlayer.play();
+        playButton.classList.add("hidden");
+        pauseButton.classList.remove("hidden");
+    });
+
+    pauseButton.addEventListener("click", () => {
+        audioPlayer.pause();
+        pauseButton.classList.add("hidden");
+        playButton.classList.remove("hidden");
+    });
 });
 
-pauseButton.addEventListener("click", () => {
-    audioPlayer.pause();
-    pauseButton.classList.add("hidden")
-    playButton.classList.remove("hidden")
-});
+
 
